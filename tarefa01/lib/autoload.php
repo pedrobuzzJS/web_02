17e309b5cb74ede78748f43d6c1189bfafd5fa6c
pedrobuzzJS <pedro.buzzi@unidavi.edu.br>
2021-02-17 15:59:15 -0300

    consertar autoload

--- tarefa01/lib/autoload.php
+++ tarefa01/lib/autoload.php
@@ -1,6 +1,6 @@
 <?php 
     spl_autoload_register(function($class){
-        $arquivo = $_SERVER["DOCUMENT_ROOT"]."/lib/".$class."_class.php";
+        $arquivo = $_SERVER["DOCUMENT_ROOT"]."./lib/".$class."_class.php";
         if (file_exists($arquivo)) {
             require $arquivo;
         }
