26fb8733b0ac013569a651a2ff85df7d4b2c2b30
pedrobuzzJS <pedro.buzzi@unidavi.edu.br>
2021-02-17 15:41:15 -0300

    autoload

---
+++ tarefa01/lib/autoload.php
@@ -0,0 +1,7 @@
+<?php 
+    spl_autoload_register(function($class){
+        $arquivo = $_SERVER["DOCUMENT_ROOT"]."/lib/".$class."_class.php";
+        if (file_exists($arquivo)) {
+            require $arquivo;
+        }
+    });
